7d6e9d9551109903bc0199d23394db49aad94886
MakussBlue <g.maciborek@wp.pl>
2019-04-18 16:41:49 +0200

    skeleton, css, js, html, Controller

--- src/main/java/project/sky_blu/Controller.java
+++ src/main/java/project/sky_blu/Controller.java
@@ -1,12 +1,53 @@
 package project.sky_blu;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @org.springframework.stereotype.Controller
 public class Controller {
 
-    @GetMapping(value = "/")
+    @GetMapping(value = "/go")
     public String index(){
         return "/index";
     }
+
+    @GetMapping(value = "/")
+    public String go(Model model){
+
+        List<String> fly = new ArrayList<>();
+
+        fly.add("Warszawa Okęcie");
+        fly.add("Warszawa Modlin");
+        fly.add("Wrocław");
+        fly.add("Poznań");
+        fly.add("Kraków");
+        fly.add("Katowice");
+        fly.add("Gdańsk");
+        fly.add("Łódź");
+        fly.add("Bydgoszcz");
+        fly.add("Rzeszów");
+        fly.add("Szczecin");
+        fly.add("Zielona Góra");
+
+
+        String [] flyy = new String[fly.size()];
+        for (int i = 0; i < fly.size(); i++) {
+            flyy[i] = fly.get(i);
+        }
+
+        model.addAttribute("lotniska", flyy);
+        return "/index";
+
+    }
+
+    @GetMapping(value="/search")
+    public String search(){
+
+
+        return "/search";
+    }
+
 }
